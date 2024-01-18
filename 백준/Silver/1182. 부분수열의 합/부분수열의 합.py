3952e13fa8d124a3d4ab6dcb70ad762dea21ba33
Rhohyunwoo <105340085+woorog@users.noreply.github.com>
2024-01-18 11:20:17 +0900

    [Silver II] Title: 부분수열의 합, Time: 368 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -8,8 +8,6 @@ cnt=0
 
 for i in range(setnum[0]):
     for combo in combinations(listnum, i+1):
-        if len(combo)==0:
-            continue
         if sum(combo)==setnum[1]:
             cnt+=1
 
