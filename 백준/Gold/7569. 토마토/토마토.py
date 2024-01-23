7b03530d6c5f1a34fc4fd799d291550ddf7f60de
Rhohyunwoo <105340085+woorog@users.noreply.github.com>
2024-01-23 16:48:42 +0900

    [Gold V] Title: 토마토, Time: 2400 ms, Memory: 57620 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,17 +1,18 @@
 import sys
 from collections import deque
 
+
 def bfs_modified(box):
     queue = deque()
-    visited = [[[-1] * m for _ in range(n)] for _ in range(h)]
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
-                if box[i][j][k] == 1:
-                    visited[i][j][k] = 0
-                    queue.append((i, j, k))
-                elif box[i][j][k] == -1:
-                    visited[i][j][k] = 0
+    visited = [[[-1] * M for _ in range(N)] for _ in range(H)]
+    for i in range(H):
+        for l in range(N):
+            for k in range(M):
+                if box[i][l][k] == 1:
+                    visited[i][l][k] = 0
+                    queue.append((i, l, k))
+                elif box[i][l][k] == -1:
+                    visited[i][l][k] = 0
     while queue:
         z, y, x = queue.popleft()
 
@@ -20,12 +21,11 @@ def bfs_modified(box):
             ny = y + dy[direction]
             nz = z + dz[direction]
 
-            if 0 <= nx < m and 0 <= ny < n and 0 <= nz < h and visited[nz][ny][nx] == -1:
+            if 0 <= nx < M and 0 <= ny < N and 0 <= nz < H and visited[nz][ny][nx] == -1:
                 queue.append((nz, ny, nx))
                 visited[nz][ny][nx] = visited[z][y][x] + 1
 
     return visited
-
 def all0(box):
     for layer in box:
         for row in layer:
@@ -34,16 +34,16 @@ def all0(box):
     return True
 
 def noway_modified(box):
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
-                if box[i][j][k] == 0:
+    for i in range(H):
+        for l in range(N):
+            for k in range(M):
+                if box[i][l][k] == 0:
                     isolated = True
                     for direction in range(6):
                         ni = i + dz[direction]
-                        nj = j + dy[direction]
+                        nl = l + dy[direction]
                         nk = k + dx[direction]
-                        if 0 <= ni < h and 0 <= nj < n and 0 <= nk < m and box[ni][nj][nk] != -1:
+                        if 0 <= ni < H and 0 <= nl < N and 0 <= nk < M and box[ni][nl][nk] != -1:
                             isolated = False
                             break
                     if isolated:
@@ -54,8 +54,13 @@ dx = [0, 0, 1, -1, 0, 0]
 dy = [1, -1, 0, 0, 0, 0]
 dz = [0, 0, 0, 0, 1, -1]
 
-m, n, h = map(int, input().split())
-box = [[list(map(int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h)]
+# 높이 H, 행 M, 열 N을 입력받습니다.
+M, N, H = map(int, input().split())
+#M, N, H = (map(int, sys.stdin.readline().split()))
+box = [[list(map(int, sys.stdin.readline().split())) for _ in range(N)] for _ in range(H)]
+
+
+
 
 if noway_modified(box):
     print(-1)
@@ -65,12 +70,12 @@ else:
     visited = bfs_modified(box)
     max_day = 0
 
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
+    for i in range(H):
+        for j in range(N):
+            for k in range(M):
                 if visited[i][j][k] == -1 and box[i][j][k] == 0:
                     print(-1)
                     sys.exit()
                 max_day = max(max_day, visited[i][j][k])
-    
+
     print(max_day)
