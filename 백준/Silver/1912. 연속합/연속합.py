62b1754aa55646ac9d38308b23262e7e8fa8ece8
Rhohyunwoo <105340085+woorog@users.noreply.github.com>
2024-01-31 10:46:10 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,27 +1,24 @@
 import sys
 
-num=int(sys.stdin.readline())
-numlist=list(map(int, sys.stdin.readline().split()))
+num = int(sys.stdin.readline())
+numlist = list(map(int, sys.stdin.readline().split()))
 numlist.append(-99999999)
-ans=numlist[0]
-dp=[numlist[0]]
-for i in range(1,num):
+ans = numlist[0]
+dp = numlist[0]
 
-    now=numlist[i]
-    if now>ans:
-        ans=now
-    cnt=0
+for i in range(1, num):
 
-    for k in dp:
-        dp[cnt]=dp[cnt]+now
-        cnt+=1
-        if ans<max(dp):
-            ans=max(dp)
+    now = numlist[i]
 
-    if max(dp)<now:
-        dp[0]=now
+    if now > ans: # 현재가 최대보다 크면 처음부터 넣어주기
+        ans = now
 
+    dp+= now  #더해서 비교
 
-
+    if ans < dp: #답이랑 지금넣은거 비교해서 크면 답으로 설정
+        ans = dp
+        
+    if dp < now:  # 현재랑 과거랑 비교해서 크면 dp에 넣어주기
+        dp= now
 
 print(ans)
